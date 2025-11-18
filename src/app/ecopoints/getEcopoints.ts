2a55262b163583d0adeb11814e9a678b35951275
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-11-18 20:24:05 -0300

    fix: ajustado chamada de ecopontos

--- src/app/ecopoints/getEcopoints.ts
+++ src/app/ecopoints/getEcopoints.ts
@@ -1,30 +1,9 @@
 import axios from '../utils/axios';
 
-export const getEcopontos = async (companyId?: string) => {
-    let resolvedCompanyId = companyId;
-    if (!resolvedCompanyId) {
-        try {
-            resolvedCompanyId = localStorage.getItem('companyId') || undefined;
-            if (!resolvedCompanyId) {
-                const userStr = localStorage.getItem('user');
-                if (userStr) {
-                    const user = JSON.parse(userStr);
-                    resolvedCompanyId = user && (user.companyId || user.id || user._id || user.userId) || undefined;
-                }
-            }
-        } catch (e) {
-            resolvedCompanyId = undefined;
-        }
-    }
-
+export const getEcopontos = async () => {
     try {
-        if (resolvedCompanyId) {
-            const response = await axios.get(`http://localhost:3001/ecopoints/my-ecopoints/${resolvedCompanyId}`);
-            return response.data;
-        } else {
-            const response = await axios.get('http://localhost:3001/ecopoints');
-            return response.data;
-        }
+        const response = await axios.get('http://localhost:3001/ecopoints');
+        return response.data;
     } catch (error) {
         console.error('Erro ao carregar ecopontos:', error);
         throw error;
