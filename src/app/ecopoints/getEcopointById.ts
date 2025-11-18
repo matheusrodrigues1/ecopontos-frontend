443a9e9d6252cbdf6c1562cfa749026b1e8d305d
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-11-18 20:56:45 -0300

    fix: ajustado busca por ecopontos da empresa

--- src/app/ecopoints/getEcopointById.ts
+++ src/app/ecopoints/getEcopointById.ts
@@ -3,6 +3,7 @@ import axios from "../utils/axios";
 export default async function getEcopointById(id: string) {
     try {
         const res = await axios.get(`http://localhost:3001/ecopoints/${id}`);
+        console.log(res);
         return res.data;
     } catch (err) {
         console.error("Erro ao buscar ecopoint por id:", err);
